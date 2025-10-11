e8a14e94b329f61e6c028b94a22def2418c335f5
joey <faisalmorensya@gmail.com>
2025-10-11 13:50:58 +0900

    Upgrade outdated dependencies

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ case "$OS" in
         echo "Detected Ubuntu/Debian"
         sudo apt-get update
         sudo apt-get install -y \
-            libwebkit2gtk-4.0-dev \
+            libwebkit2gtk-4.1-dev \
             build-essential \
             curl \
             wget \
